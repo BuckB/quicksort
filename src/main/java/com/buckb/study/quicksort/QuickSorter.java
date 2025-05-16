885c9b818e48f1251b29e9fe5250ff2ffc912c60
Bruno Buck <b.brigagao@gmail.com>
2025-05-16 14:07:11 -0300

    Write minimal code to make the test pass.

--- src/main/java/com/buckb/study/quicksort/QuickSorter.java
+++ src/main/java/com/buckb/study/quicksort/QuickSorter.java
@@ -3,6 +3,9 @@ package com.buckb.study.quicksort;
 public class QuickSorter {
 
     public void sort(int[] array) {
+        if (array == null) {
+            throw new IllegalArgumentException("Input array cannot be null");
+        }
     }
 
 }
