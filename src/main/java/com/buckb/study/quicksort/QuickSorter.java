89b3bfe40214f66995d32f07baeba24b9a8f4837
Bruno Buck <b.brigagao@gmail.com>
2025-05-16 13:14:31 -0300

    First use case: given null array then IllegalArgumentException is thown: red

--- src/main/java/com/buckb/study/quicksort/QuickSorter.java
+++ src/main/java/com/buckb/study/quicksort/QuickSorter.java
@@ -2,4 +2,7 @@ package com.buckb.study.quicksort;
 
 public class QuickSorter {
 
+    public void sort(int[] array) {
+    }
+
 }
--- src/test/java/com/buckb/study/quicksort/QuickSorterTest.java
+++ src/test/java/com/buckb/study/quicksort/QuickSorterTest.java
@@ -1,9 +1,27 @@
 package com.buckb.study.quicksort;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuickSorterTest {
+
     private QuickSorter quickSorter;
 
+    @BeforeEach
     public void setup() {
         this.quickSorter = new QuickSorter();
+    }
+
+    @Test
+    @DisplayName("Given a null array, when sort is called, then an IllegalArgumentException should be thrown")
+    void givenNullArray_whenSort_thenThrowIllegalArgumentException() {
+        int[] array = null;
+
+        assertThatThrownBy(() -> this.quickSorter.sort(array))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Input array cannot be null");
     }
 }
