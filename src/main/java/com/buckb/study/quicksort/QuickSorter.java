5af097266bd9f4f4d7b80c36d972275de9e8933c
Bruno Buck <b.brigagao@gmail.com>
2025-05-18 14:32:23 -0300

    Use Case 5: Refactoring

--- src/main/java/com/buckb/study/quicksort/QuickSorter.java
+++ src/main/java/com/buckb/study/quicksort/QuickSorter.java
@@ -10,11 +10,15 @@ public class QuickSorter {
             return array;
         }
         if (array[0] > array[1]) {
-            int temp = array[0];
-            array[0] = array[1];
-            array[1] = temp;
+            this.swap(array, 0, 1);
         }
         return array;
+    }
+
+    private void swap(int[] array, int greaterValueIndex, int lowerValueIndex) {
+        int temp = array[greaterValueIndex];
+        array[greaterValueIndex] = array[lowerValueIndex];
+        array[lowerValueIndex] = temp;
     }
 
 }
