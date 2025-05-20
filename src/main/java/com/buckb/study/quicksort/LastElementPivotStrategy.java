0631c7f0d2dcdc1e4181d898d0ba3d4ee5168e06
Bruno Buck <b.brigagao@gmail.com>
2025-05-20 17:17:22 -0300

    Fixing the pivot strategy implementation

--- src/main/java/com/buckb/study/quicksort/LastElementPivotStrategy.java
+++ src/main/java/com/buckb/study/quicksort/LastElementPivotStrategy.java
@@ -4,7 +4,7 @@ public class LastElementPivotStrategy implements PivotStrategy {
 
     @Override
     public int choosePivot(int[] array, int low, int high) {
-        return array[high];
+        return high;
     }
 
 }
--- src/main/java/com/buckb/study/quicksort/PivotStrategy.java
+++ src/main/java/com/buckb/study/quicksort/PivotStrategy.java
@@ -1,5 +1,15 @@
 package com.buckb.study.quicksort;
 
 public interface PivotStrategy {
+    /**
+     * Chooses and returns the index of the pivot element within the specified sub-array.
+     * This method is responsible for determining *where* the pivot is, not for its value
+     * or for physically moving it within the array.
+     *
+     * @param array The array being sorted.
+     * @param low The starting index of the sub-array.
+     * @param high The ending index of the sub-array.
+     * @return The index of the chosen pivot element.
+     */
     int choosePivot(int[] array, int low, int high);
 }
